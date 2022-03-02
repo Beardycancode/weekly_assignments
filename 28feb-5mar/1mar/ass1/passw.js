71f0164fff5f9833ae8a8658c294034c4ea9b388
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-03-02 09:05:21 +0530

    Update and rename pass.js to passw.js

--- 28feb-5mar/1mar/ass1/passw.js
+++ 28feb-5mar/1mar/ass1/passw.js
@@ -1,13 +1,8 @@
 function checkPassword(form) {
     password1 = form.password1.value;
     password2 = form.password2.value;
-    if (password1 == '') {
-        alert("Please enter Password");
-    }
-    else if (password2 == '') {
-        alert("Please confirm password");
-    }
-    else if (password1 != password2) {
+   
+    if (password1 != password2) {
         alert("\nPassword did not match: Please enter again")
         return false;
     }
