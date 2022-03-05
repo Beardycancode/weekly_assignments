dd7ade28878cf93f8c69b6c3d7ed2601b3e916e4
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-03-05 22:14:47 +0530

    Update swap.js

--- 28feb-5mar/3mar/ass2/swap.js
+++ 28feb-5mar/3mar/ass2/swap.js
@@ -1,13 +1,4 @@
-    let a = 20;
-    let b = 10;
-    document.write('Your entries are: 20 and 10<br /><br /><br /> ')
-
-    //creating a temporary variable;
-    let temp;
-
-    //swap variables by assigning values;
-    temp = a;
-    a = b;
-    b = temp;
-
-    document.write(`The values after swapping are: ${a} ${b}`);
+let a = 10;
+let b = 20;
+[a, b] = [b, a];
+document.write(`<h3>value of a is   ${a} and value of b is ${b}</h3>`)
